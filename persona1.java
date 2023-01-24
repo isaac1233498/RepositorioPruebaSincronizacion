f0dfbe410e3dcf23cc14d28c4a5527eeb6210c4c
isaac <isaac1233@hotmail.com>
2023-01-24 00:37:47 -0300

    ingresando cambio en java para fusionar ramas

--- persona1.java
+++ persona1.java
@@ -6,5 +6,7 @@ public class persona1 {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         //probando guardar version
+
+        //Linea para ver si se fusionan las ramas
     }
 }
